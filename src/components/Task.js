cbeb96483f926e48d77465acd4cc7cba0de8181e
Nikhil <nikhil13012000@gmail.com>
2020-05-24 13:18:08 +0530

    accept changes

--- src/components/Task.js
+++ src/components/Task.js
@@ -22,7 +22,7 @@ export default function Task({
                     onClick={() => onArchiveTask(id)}
                 />
             </label>
-            <div className='title'>
+            <div className='title' style={{ background: 'red' }}>
                 <input
                     type='text'
                     value={title}

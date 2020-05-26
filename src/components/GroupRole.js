2d31d59c1c50c9e9ee0a54e9f6cc5009f14acfe5
Nikhil <nikhil13012000@gmail.com>
2020-05-26 11:39:10 +0530

    refactored to functional component

--- src/components/GroupRole.js
+++ src/components/GroupRole.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { initialStateData } from './GroupRole.stories';
+import React from 'react';
 import { Grid, Card } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
+import PropTypes from 'prop-types';
+
 const classes = {
     card_style: {
         margin: '10px 0px',
@@ -11,40 +12,33 @@ const classes = {
     },
 };
 
-class GroupRole extends Component {
-    constructor(props) {
-        super(props);
-        // initial state comes from storybook else provide the default initial state
-        this.state = props.stateData ? props.stateData : initialStateData;
-    }
-    render() {
-        return (
-            <Grid
-                container
-                direction='column'
-                justify='center'
-                alignItems='center'>
-                {this.state.loading ? (
-                    <Grid item>
-                        <Card style={classes.card_style}>
-                            <h3>Role</h3>
-                            <hr />
-                            <Skeleton variant='text' animation='wave' />
-                            <Skeleton variant='text' animation='wave' />
-                        </Card>
-                    </Grid>
-                ) : (
-                    <Grid item>
-                        <Card style={classes.card_style}>
-                            <h3>Role</h3>
-                            <hr />
-                            <p>{this.state.role}</p>
-                        </Card>
-                    </Grid>
-                )}
-            </Grid>
-        );
-    }
-}
+const GroupRole = ({ stateData: { loading, role } }) => {
+    return (
+        <Grid container direction='column' justify='center' alignItems='center'>
+            {loading ? (
+                <Grid item>
+                    <Card style={classes.card_style}>
+                        <h3>Role</h3>
+                        <hr />
+                        <Skeleton variant='text' animation='wave' />
+                        <Skeleton variant='text' animation='wave' />
+                    </Card>
+                </Grid>
+            ) : (
+                <Grid item>
+                    <Card style={classes.card_style}>
+                        <h3>Role</h3>
+                        <hr />
+                        <p>{role}</p>
+                    </Card>
+                </Grid>
+            )}
+        </Grid>
+    );
+};
+
+GroupRole.propTypes = {
+    stateData: PropTypes.object,
+};
 
 export default GroupRole;
